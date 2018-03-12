ce6fc8c8eb786a52b66d0010340b034752417ccd
Laura Jodz <laurajodz@gmail.com>
2018-03-12 13:50:03 -0700

    fix typo

--- src/components/login.js
+++ src/components/login.js
@@ -1,53 +1,23 @@
 import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
-import Input from './input';
-import {login} from '../actions/auth';
-import {required, nonEmpty} from '../validators';
 import {Link} from 'react-router-dom';
 
 // import './login.css';
 
 export default function Login(props) {
-    {/*onSubmit(values) {
-        return this.props.dispatch(login(values.username, values.password));
-    }*/}
-
-    let error;
-    if (this.props.error) {
-        error = (
-            <div className="form-error" aria-live="polite">
-                {this.props.error}
-            </div>
-        );
-    }
 
     return (
 
-        <form
-            className="login">
-            onSubmit={this.props.handleSubmit(values =>
-            this.onSubmit(values)
-            )}>
-            {error}
-            <label htmlFor="username">Username</label>
-            <Field
-                component={Input}
-                type="text"
-                name="username"
-                id="username"
-                validate={[required, nonEmpty]}
-            />
-            <label htmlFor="password">Password</label>
-            <Field
-                component={Input}
-                type="password"
-                name="password"
-                id="password"
-                validate={[required, nonEmpty]}
-            />
-            <button disabled={this.props.pristine || this.props.submitting}>
-                Log in
-            </button>
+        <form className="login">
+
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name='username' id='username' />
+            </div>
+
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name='password' id='password' />
+            </div>
 
             <button
                 className="login-button"
