ff684326433d3da884063ffac6301edd5d9b4476
Laura Jodz <laurajodz@gmail.com>
2018-03-28 22:11:15 -0700

    edit function

--- src/components/add-entry-card.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import AddEntryCard from './add-entry-card';
-
-describe('<AddEntryCard />', () => {
-    it('Renders without crashing', () => {
-        shallow(<AddEntryCard />);
-    });
-});
